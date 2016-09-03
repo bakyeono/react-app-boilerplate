d703ba0b293703f5c2714e3b2990193835b85659
bakyeono <bakyeono@gmail.com>
2016-09-03 19:56:58 +0900

    Add grocery list

--- app/App.js
+++ app/App.js
@@ -1,12 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {render} from 'react-dom';
 
 class App extends Component {
-  render(){
-    return (
-      <h1>Hello World</h1>
-    );
-  }
+    render() {
+        return (
+            <h1>Hello World</h1>
+        );
+    }
 }
 
-render(<App />, document.getElementById('root'));
+class Hello extends Component {
+    render() {
+        var place = 'Seoul';
+        return (
+            <h1>Hello {place}</h1>
+        );
+    }
+}
+
+class GroceryList extends Component {
+    render() {
+        return (
+            <ul>
+                <ListItem quantity="1">Bread</ListItem>
+                <ListItem quantity="6">Eggs</ListItem>
+                <ListItem quantity="2">Milk</ListItem>
+            </ul>
+        );
+    }
+}
+
+class ListItem extends Component {
+    render() {
+        return (
+            <li>
+                {this.props.quantity} x {this.props.children}
+            </li>
+        );
+    }
+}
+
+render(<GroceryList />, document.getElementById('root'));
