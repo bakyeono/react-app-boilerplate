d65c9d5a994ee14a40a303afaf8abb0f10fb4953
bakyeono <bakyeono@gmail.com>
2016-09-04 22:39:54 +0900

    Pass methods and bind them

--- app/CheckList.js
+++ app/CheckList.js
@@ -1,21 +1,47 @@
 import React, {Component, PropTypes} from 'react';
 
 class CheckList extends Component {
+  checkInputKeyPress(event) {
+    if (event.key === 'Enter') {
+      this.props.taskCallbacks.add(this.props.cardId, event.target.value);
+      event.target.value = '';
+    }
+  }
+
   render() {
-    let tasks = this.props.tasks.map((task) => (
+    let tasks = this.props.tasks.map((task, taskIndex) => (
       <li key={task.id} className="checklist__task">
-        <input type="checkbox" defaultChecked={task.done} />
-          {task.name}
-          <a href="#" className="checklist__task--remove" />
-        </li>
+        <input
+          type="checkbox"
+          checked={task.done}
+          onChange={
+            this.props.taskCallbacks.toggle.bind(
+              null, this.props.cardId, task.id, taskIndex
+            )
+          }
+          />
+        {task.name}
+        <a
+          href="#"
+          className="checklist__task--remove"
+          onClick={
+            this.props.taskCallbacks.delete.bind(
+              null, this.props.cardId, task.id, taskIndex
+            )
+          }
+          />
+      </li>
     ));
     return (
       <div className="checkList">
         <ul>{tasks}</ul>
-        <input type="text"
-            className="checklist--add-task"
-            placeholder="Type then hit Enter to add a task" />
-        </div>
+        <input
+          type="text"
+          className="checklist--add-task"
+          placeholder="Type then hit Enter to add a task"
+          onKeyPress={this.checkInputKeyPress.bind(this)}
+          />
+      </div>
     );
   }
 }
--- app/List.js
+++ app/List.js
@@ -3,13 +3,14 @@ import Card from './Card';
 
 class List extends Component {
   render() {
-    var cards = this.props.cards.map((card) => {
-      return <Card key={card.id}
-          id={card.id}
-          title={card.title}
-          description={card.description}
-          color={card.color}
-          tasks={card.tasks} />
+    let cards = this.props.cards.map((card) => {
+      return (
+        <Card
+          key={card.id}
+          taskCallbacks={this.props.taskCallbacks}
+          {...card}
+          />
+      )
     });
     return (
       <div className="list">
@@ -22,7 +23,8 @@ class List extends Component {
 
 List.propTypes = {
   title: PropTypes.string.isRequired,
-  cards: PropTypes.arrayOf(PropTypes.object)
+  cards: PropTypes.arrayOf(PropTypes.object),
+  taskCallbacks: PropTypes.object
 };
 
 export default List;
