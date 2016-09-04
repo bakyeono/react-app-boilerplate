42d99976fc98d1f5098e292bc0a1b3129ac7b842
bakyeono <bakyeono@gmail.com>
2016-09-04 17:55:02 +0900

    Fix indent

--- app/App.js
+++ app/App.js
@@ -3,39 +3,39 @@ import {render} from 'react-dom';
 import KanbanBoard from './KanbanBoard';
 
 let cardList = [
-    {
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the **whole** book",
+    color: '#bd8d31',
+    status: "in-progress",
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "Write some code",
+    description: "Code along with the samples in the book.\n" +
+      "The complete source can be found at [github](https://github.com/pro-react)",
+    color: '#3a7e28',
+    status: "todo",
+    tasks: [
+      {
         id: 1,
-        title: "Read the Book",
-        description: "I should read the **whole** book",
-        color: '#bd8d31',
-        status: "in-progress",
-        tasks: []
-    },
-    {
+        name: "ContactList Example",
+        done: true
+      },
+      {
         id: 2,
-        title: "Write some code",
-        description: "Code along with the samples in the book.\n" +
-        "The complete source can be found at [github](https://github.com/pro-react)",
-        color: '#3a7e28',
-        status: "todo",
-        tasks: [
-            {
-                id: 1,
-                name: "ContactList Example",
-                done: true
-            },
-            {
-                id: 2,
-                name: "Kanban Example",
-                done: false
-            },
-            {
-                id: 3,
-                name: "My own experiments",
-                done: false
-            }
-        ]
-    }
+        name: "Kanban Example",
+        done: false
+      },
+      {
+        id: 3,
+        name: "My own experiments",
+        done: false
+      }
+    ]
+  }
 ];
 
 render(<KanbanBoard cards={cardList} />, document.getElementById('root'));
