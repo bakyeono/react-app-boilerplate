bab9dfc65981b1ff3400583b629427cb92985da2
bakyeono <bakyeono@gmail.com>
2016-09-04 17:43:02 +0900

    Add custom prop type

--- app/Card.js
+++ app/Card.js
@@ -2,6 +2,17 @@ import React, {Component, PropTypes} from 'react';
 import CheckList from './CheckList';
 import marked from 'marked';
 
+let titlePropType = (props, propName, componentName) => {
+    if (props[propName]) {
+        let value = props[propName];
+        if ((typeof value) !== 'string' || 80 < value.length) {
+            return new Error(
+                '${propName} in ${componentName} is longer than 80 characters'
+            );
+        }
+    }
+};
+
 class Card extends Component {
     constructor() {
         super(...arguments);
@@ -20,7 +31,7 @@ class Card extends Component {
             cardDetails = (
                 <div className="card__details">
                     <span dangerouslySetInnerHTML={{__html:marked(this.props.description)}} />
-                    <CheckList cardId="this.props.id" tasks={this.props.tasks} />
+                    <CheckList cardId={this.props.id} tasks={this.props.tasks} />
                 </div>
             )
         }
@@ -51,7 +62,7 @@ class Card extends Component {
 
 Card.propTypes = {
     id: PropTypes.number,
-    title: PropTypes.string,
+    title: titlePropType,
     description: PropTypes.string,
     color: PropTypes.string,
     tasks: PropTypes.arrayOf(PropTypes.object)
